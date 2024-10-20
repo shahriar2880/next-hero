a698d3bacd9dfdf4dabfe74c713c335eed56486a
shahriar2880 <shahriarreza436@gmail.com>
2024-10-20 18:21:07 +0600

    next js routes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-red-500 px-6 py-4">
+    <h6>Logo</h6>
+    <ul>
+      <li>Home</li>
+      <li>Services</li>
+      <li>About</li>
+    </ul>
+  </nav>
+  )
+}
+
+export default Navbar
