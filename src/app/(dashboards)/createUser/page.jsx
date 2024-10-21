7354d35e60fdc12e5b185f8be0d98c14568b1fec
shahriar2880 <shahriarreza436@gmail.com>
2024-10-21 20:48:35 +0600

    group route

---
+++ src/app/(dashboards)/createUser/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen'>
+      <h1 className='text-center'>Create User Dashboard</h1>
+    </div>
+  )
+}
+
+export default page
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,56 @@
-"use client"
-import Link from 'next/link'
-import { usePathname, useRouter } from 'next/navigation'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
 
 const Navbar = () => {
-    const pathName = usePathname();
-    const router = useRouter();
-    const links = [
-        {
-            title : "About",
-            path : '/about'
-        },
-        {
-            title : "Services",
-            path : '/services'
-        },
-        {
-            title : "Contact",
-            path : '/contacts'
-        },
-        {
-            title : "Blogs",
-            path : '/blogs'
-        },
-    ]
-    const handlerLogin =() =>{
-        router.push('/login')
-    }
+  const pathName = usePathname();
+  const router = useRouter();
+  const links = [
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Services",
+      path: "/services",
+    },
+    {
+      title: "Contact",
+      path: "/contacts",
+    },
+    {
+      title: "Blogs",
+      path: "/blogs",
+    },
+  ];
+  const handlerLogin = () => {
+    router.push("/login");
+  };
+
+  if (pathName.includes("dashboard")) return <div>Dashboard Layout</div>;
+
   return (
     <nav className="bg-red-500 px-6 py-4 flex justify-between items-center">
-    <h6 className='text-3xl'>Next <span className='text-cyan-700'>Hero</span></h6>
-    <ul className='flex justify-between items-center space-x-4'>
-      {
-        links?.map((link) => <Link className={`${pathName === link.path && "text-cyan-700"}`} key={link.path} href={link.path}>{link.title}</Link>)
-      }
-    </ul>
-    <button onClick={handlerLogin} className='bg-white text-cyan-700 p-3'>LogIn</button>
-  </nav>
-  )
-}
+      <h6 className="text-3xl">
+        Next <span className="text-cyan-700">Hero</span>
+      </h6>
+      <ul className="flex justify-between items-center space-x-4">
+        {links?.map((link) => (
+          <Link
+            className={`${pathName === link.path && "text-cyan-700"}`}
+            key={link.path}
+            href={link.path}
+          >
+            {link.title}
+          </Link>
+        ))}
+      </ul>
+      <button onClick={handlerLogin} className="bg-white text-cyan-700 p-3">
+        LogIn
+      </button>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
