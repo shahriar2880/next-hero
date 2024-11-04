1c4cb0384a64cfd350553ef029444b50f96f092d
shahriar2880 <shahriarreza436@gmail.com>
2024-11-05 02:14:02 +0600

    setup credentials email provider

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+
+const handler = NextAuth({
+    session: {
+        strategy : 'jwt'
+    },
+    providers : [
+
+    ],
+});
+
+export {handler as GET, handler as POST}
