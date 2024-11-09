2a8c81432cded2738c9a6cfd2cb8cb511dab9e21
shahriar2880 <shahriarreza436@gmail.com>
2024-11-10 05:13:01 +0600

    sign up with email and password

---
+++ src/app/api/auth/[...nextauth]/page.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const page = () => {
+    const handelRegister = async (event) => {
+
+    };
+  return (
+    <div className='flex items-center justify-center mt-24'>
+      <div>
+        <h6 className='text-lg font-semibold'>
+            Sign Up With Email And Password
+        </h6>
+        <div>
+            <form onSubmit={handelRegister} action=''>
+                <label htmlFor='email'> Email </label><br/>
+                <input
+                type='text'
+                name='email'
+                placeholder='your email'
+                className='outline-none border-transparent p-3 text-slate-700'
+                /> <br/><br/>
+                <label htmlFor='password'> Password </label><br/>
+                <input
+                type='password'
+                name='password'
+                placeholder='your password'
+                className='outline-none border-transparent p-3 text-slate-700'
+                /> <br/>
+                <button className='bg-orange-500 p-3'>Register</button>
+            </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
