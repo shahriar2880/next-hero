f6f5adaa00f1ee4b5b4145edf7a7bed8fc9da408
shahriar2880 <shahriarreza436@gmail.com>
2024-11-13 17:34:12 +0600

    get user

--- src/app/api/auth/signup/new-user/route.js
+++ src/app/api/auth/signup/new-user/route.js
@@ -1,7 +1,13 @@
+import connectDB from "@/lib/connectDB"
+
 export const POST = async () =>{
     try {
-        
+        const db = await connectDB();
+        const userCollection = db.collection('users');
+        const newUser = await request.json();
+        const res = await userCollection.insertOne(newUser);
+        return Response.json({message: "new user created"})
     } catch (error) {
-        
+        return Response.json({message: "Something Went Wrong"})
     }
 }
--- src/app/api/auth/signup/page.jsx
+++ src/app/api/auth/signup/page.jsx
@@ -1,45 +1,71 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 
 const page = () => {
-    const handelRegister = async (event) => {
-        event.preventDefault();
+  const handelRegister = async (event) => {
+    event.preventDefault();
+    const newUser = {
+      name: event.target.name.value,
+      email: event.target.email.value,
+      password: event.target.password.value,
     };
+    const resp = await fetch(
+      "http://localhost:3000/api/auth/signup/new-user",
+      {
+        method: "POST",
+        body : JSON.stringify(newUser),
+        headers: {
+          "content-type" : "application/json",
+        },
+      }
+    );
+    console.log(resp)
+  };
+
   return (
-    <div className='flex items-center justify-center min-h-screen top-0'>
+    <div className="flex items-center justify-center min-h-screen top-0">
       <div>
-        <h3 className='text-lg font-semibold'>
-            Sign Up With Email And Password
+        <h3 className="text-lg font-semibold">
+          Sign Up With Email And Password
         </h3>
         <div>
-            <form onSubmit={handelRegister} action=''>
-                <label htmlFor='email'> Name </label><br/>
-                <input
-                type='text'
-                name='name'
-                placeholder='your name'
-                className='outline-none border-transparent p-3 text-slate-700'
-                /> <br/><br/>
-                <label htmlFor='email'> Email </label><br/>
-                <input
-                type='text'
-                name='email'
-                placeholder='your email'
-                className='outline-none border-transparent p-3 text-slate-700'
-                /> <br/><br/>
-                <label htmlFor='password'> Password </label><br/>
-                <input
-                type='password'
-                name='password'
-                placeholder='your password'
-                className='outline-none border-transparent p-3 text-slate-700'
-                /> <br/><br/>
-                <button className='bg-orange-500 p-3'>Register</button>
-            </form>
+          <form onSubmit={handelRegister} action="">
+            <label htmlFor="email"> Name </label>
+            <br />
+            <input
+              type="text"
+              name="name"
+              placeholder="your name"
+              className="outline-none border-transparent p-3 text-slate-700"
+            />{" "}
+            <br />
+            <br />
+            <label htmlFor="email"> Email </label>
+            <br />
+            <input
+              type="text"
+              name="email"
+              placeholder="your email"
+              className="outline-none border-transparent p-3 text-slate-700"
+            />{" "}
+            <br />
+            <br />
+            <label htmlFor="password"> Password </label>
+            <br />
+            <input
+              type="password"
+              name="password"
+              placeholder="your password"
+              className="outline-none border-transparent p-3 text-slate-700"
+            />{" "}
+            <br />
+            <br />
+            <button className="bg-orange-500 p-3">Register</button>
+          </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
